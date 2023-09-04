f461fa3d9dc59dffda6616c3f98585778bdf5336
bluefirerose <rose.a.exile@gmail.com>
2023-09-04 19:21:50 +0800

    updated routes

--- java-angular-frontend-main/src/app/category/create/create.component.ts
+++ java-angular-frontend-main/src/app/category/create/create.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../category.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-      
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-     
+
   form!: FormGroup;
-     
+
   /*------------------------------------------
   --------------------------------------------
   Created constructor
@@ -21,7 +21,7 @@ export class CreateComponent implements OnInit {
     public categoryService: CategoryService,
     private router: Router
   ) { }
-     
+
   /**
    * Write code on Method
    *
@@ -36,7 +36,7 @@ export class CreateComponent implements OnInit {
       //description: new FormControl('', Validators.required)
     });
   }
-     
+
   /**
    * Write code on Method
    *
@@ -45,7 +45,7 @@ export class CreateComponent implements OnInit {
   get f(){
     return this.form.controls;
   }
-     
+
   /**
    * Write code on Method
    *
@@ -55,9 +55,9 @@ export class CreateComponent implements OnInit {
     console.log(this.form.value);
     this.categoryService.create(this.form.value).subscribe((res:any) => {
          console.log('Product created successfully!');
-         this.router.navigateByUrl('product/index');
+         this.router.navigateByUrl('category/patient');
     })
   }
-  
-   
+
+
 }
